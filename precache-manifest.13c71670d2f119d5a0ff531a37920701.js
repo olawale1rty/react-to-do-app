ca221d4c59fc5bdf1b07de3fd1ce3f69366edc6e
Mustapha Moshood <olawale3415@gmail.com>
2020-07-12 03:39:16 +0100

    Updates

---
+++ precache-manifest.13c71670d2f119d5a0ff531a37920701.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8d26608520d68d9df6d838700f20f0a",
+    "url": "/react-to-do-app/index.html"
+  },
+  {
+    "revision": "327c74157c7837daba09",
+    "url": "/react-to-do-app/static/css/main.d126c335.chunk.css"
+  },
+  {
+    "revision": "45ef5d82fc7a81d203c8",
+    "url": "/react-to-do-app/static/js/2.34112bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-to-do-app/static/js/2.34112bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "327c74157c7837daba09",
+    "url": "/react-to-do-app/static/js/main.7cbcf42c.chunk.js"
+  },
+  {
+    "revision": "1f0f21563b9311b40c95",
+    "url": "/react-to-do-app/static/js/runtime-main.84d05ee3.js"
+  }
+]);
